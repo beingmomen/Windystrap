72689037ad7ea9cb7c5ed6edb55ab2cf88b11e2e
beingmomen <beingmomen@demo.com>
2023-01-14 12:54:18 +0200

    push

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,10 @@ import vue from "@vitejs/plugin-vue";
 
 // i18n
 import { resolve, dirname } from "node:path";
-// import vueI18n from "@intlify/vite-plugin-vue-i18n";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    // vueI18n({
-    //   // you need to set i18n resource including paths
-    //   // include: resolve(dirname(fileURLToPath(import.meta.url)), "../locales"),
-    // }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
